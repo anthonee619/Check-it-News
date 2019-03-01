ef9f5daf90ad7f796a64281deb34ee1868bfd138
cdclaw <langluo9843@gmail.com>
2019-02-28 22:31:05 -0800

    front end completed

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ let bgPage = chrome.extension.getBackgroundPage();
 let urls = bgPage.urls;
 
 for (var i = 0; i < urls.length; i++) {
-  $("#relatedNews").append('<li><a href="'+ urls[i].url + '" target="+_blank">' + urls[i].title + '</a></li>');
+  $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank">' + urls[i].title + '</a></li>');
 }
