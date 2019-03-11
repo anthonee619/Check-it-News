08d4305944a8c0892e319ba41e2c090659b5b057
John Luke Saake <johnlukesaake@gmail.com>
2019-03-11 16:38:26 -0700

    Bug Fixing
    Fixed bug where info would disappear due to scoping issues in document ready.

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
 console.log("Hello World this is the content_scripts");
 
+var info = {title: "", host: ""};
+
 $(document).ready(function() {
   console.log("Document ready");
   var docTitle = document.title;
   var hostWebsite = window.location.hostname;
 
-  var info = {title: docTitle, host: hostWebsite};
+  info = {title: docTitle, host: hostWebsite};
 
   chrome.runtime.sendMessage(info);
   //updateData(info);
