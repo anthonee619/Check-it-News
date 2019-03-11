81aa8dae4598a425d117fe470ae24f2399a720c0
Roland A. Fernando <36901178+anthonee619@users.noreply.github.com>
2019-03-11 16:03:38 -0700

    SamePage
    Updates so we are on the same page

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log("hello");
 
 let bgPage = chrome.extension.getBackgroundPage();
-let urls = bgPage.news;
-console.log(urls);
-
-for (var i = 0; i < 5; i++) {
-  $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank" title="'+ urls[i].title + '">' + urls[i].source.name + '</a></li>');
-}
+// let urls = bgPage.news;
+// console.log(urls);
+//
+// for (var i = 0; i < 5; i++) {
+//   $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank" title="'+ urls[i].title + '">' + urls[i].source.name + '</a></li>');
+// }
