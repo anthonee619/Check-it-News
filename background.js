650ec1b32cd26840e00eba7aba637a6c0c733e39
Roland A. Fernando <36901178+anthonee619@users.noreply.github.com>
2019-03-11 13:27:50 -0700

    Update background.js
    Cleaner and fixed makeQuery to not include source

--- background.js
+++ background.js
@@ -5,29 +5,26 @@ chrome.runtime.onMessage.addListener(searchResults);
 window.urls = {};
 
 function searchResults(req, sender, sendResponse){
-  // let query = req.title
-  // console.log(req);
-  console.log(encodeURI(req.title));
-  console.log(encodeURI(makeQuery(req.title)));
-  var jsonResponse = search(encodeURI(makeQuery(req.title)));
-  console.log(jsonResponse);
+  console.log(req);
+  console.log(makeQuery(req.title));
+  search(makeQuery(req.title));
 }
 
 function makeQuery(text){
-  return text.replace(/\s+/g, '+');
+  let temp = text.split(/\?|&|\/|\||-|_/g);
+  let title = '';
+  for (var i in temp) {
+    if (temp[i].length > title) {
+      title = temp[i];
+    }
+  }
+  return encodeURI(title.replace(/\s+/g, '+'));
 }
 
 function search(query) {
-  // uses the contentwebsearch news api to get relevant news stories.
-  // const uri = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/NewsSearchAPI?q=";
   const uri = "https://newsapi.org/v2/everything/?q=";
-  // const autoCorrect = "&autoCorrect=true";
-  // const pageNumber = "&pageNumber=1";
-  // const pageSize = "&pageSize=10";
-  // const safeSearch = "&safeSearch=false";
 
   let head = new Headers();
-  // head.append('X-RapidAPI-Key', '93c7922464mshb212bc584276a9ep13f7a4jsn79ab8987bec6');
   head.append('X-Api-Key', 'e32f1d5feb3f4027851ca426d3b3b2e3');
   let req = new Request(uri + encodeURI(query), {
     method: 'GET',
@@ -41,8 +38,7 @@ function search(query) {
       throw new Error('Bad HTTP stuff');
     }
   }).then((jsonData) => {
-    console.log(jsonData);
-    return jsonData;
+    window.news = jsonData.articles;
   }).catch((err) => {
     console.log('Error: ', err.message);
   });
