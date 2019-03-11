a3f9dc9a6653627750d35db41cf53f834e9887c2
Roland A. Fernando <36901178+anthonee619@users.noreply.github.com>
2019-03-11 14:14:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 console.log("hello");
 
 let bgPage = chrome.extension.getBackgroundPage();
-let urls = bgPage.urls;
+let urls = bgPage.news;
+// console.log(urls);
 
-for (var i = 0; i < urls.length; i++) {
-  $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank">' + urls[i].title + '</a></li>');
+for (var i = 0; i < 5; i++) {
+  console.log(urls[1]);
+  $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank">' + urls[i].title.substring(0,30) + '</a></li>');
 }
