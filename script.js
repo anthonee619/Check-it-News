edf2322fca8568040fd6494e0b9ec8f80d3f63ea
Roland A. Fernando <36901178+anthonee619@users.noreply.github.com>
2019-03-11 14:50:21 -0700

    Update script.js
    Source names and titles when hover!

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ console.log("hello");
 
 let bgPage = chrome.extension.getBackgroundPage();
 let urls = bgPage.news;
-// console.log(urls);
+console.log(urls);
 
 for (var i = 0; i < 5; i++) {
-  console.log(urls[1]);
-  $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank">' + urls[i].title.substring(0,30) + '</a></li>');
+  $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank" title="'+ urls[i].title + '">' + urls[i].source.name + '</a></li>');
 }
