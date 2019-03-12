9080f7763faac9d2cf02e806f7a8aca2997707d0
Roland A. Fernando <36901178+anthonee619@users.noreply.github.com>
2019-03-11 21:26:23 -0700

    cleanUp
    -Changed to the correct icons.
    -Related articles uncommented

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 console.log("hello");
 
 let bgPage = chrome.extension.getBackgroundPage();
-// let urls = bgPage.news;
-// console.log(urls);
-//
-// for (var i = 0; i < 5; i++) {
-//   $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank" title="'+ urls[i].title + '">' + urls[i].source.name + '</a></li>');
-// }
-
 console.log($("#type").text());
 updateData(bgPage.info);
+let urls = bgPage.news;
+console.log(urls);
+
+for (var i = 0; i < 5; i++) {
+  $("#relatedNews").append('<li><a id="related-link"href="'+ urls[i].url + '" target="+_blank" title="'+ urls[i].title + '">' + urls[i].source.name + '</a></li>');
+}
